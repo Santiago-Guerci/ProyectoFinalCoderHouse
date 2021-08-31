f02c83f86fbef9aa0a28b1563dac6f3fe11109af
Santiago-Guerci <santiago.38@hotmail.com>
2021-08-31 16:04:23 -0300

    arreglo nuevamente php

--- enviar.php
+++ enviar.php
@@ -13,7 +13,7 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre $apellido . ",\r\n";
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $email . " \r\n";
 $mensaje .= "Es de: " . $ciudad $provincia . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
